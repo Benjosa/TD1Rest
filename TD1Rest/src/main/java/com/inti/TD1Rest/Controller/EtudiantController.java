ca772b7c4c61029f0baab4032778f84991ac63ac
jonathansablier <jonathansablier@macbook-pro-de-jonathan-2.home>
2022-05-30 14:24:50 +0200

    modification de la fonction delete

--- TD1Rest/src/main/java/com/inti/TD1Rest/Controller/EtudiantController.java
+++ TD1Rest/src/main/java/com/inti/TD1Rest/Controller/EtudiantController.java
@@ -9,11 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController  //WEB SERVICE
@@ -67,19 +69,19 @@ public class EtudiantController
 	
 	
 	
-	@GetMapping("/deleteEtudiant")
-	public String deleteEtudiant()
+	@DeleteMapping("/deleteEtudiant")
+	public String deleteStudent(@RequestParam("id") int id)
 	{
-		er.deleteById(1);
+		er.deleteById(id);
 		
-		return "Etudiant Supprimer";
+		return "Student delete";
 	}
 	
 	@GetMapping("/getEtudiant")
 	public Etudiant getEtudiant()
 	{
 		
-		return er.getById(3);
+		return er.getReferenceById(3);
 	}
 	
 	
