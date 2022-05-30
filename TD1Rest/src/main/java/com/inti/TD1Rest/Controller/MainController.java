44aaee06d1b993611681cfb0e162ef5991c51600
jonathansablier <jonathansablier@macbook-pro-de-jonathan-2.home>
2022-05-30 17:05:15 +0200

    ajout de la fonction d'association entre professeur et ecole

--- TD1Rest/src/main/java/com/inti/TD1Rest/Controller/MainController.java
+++ TD1Rest/src/main/java/com/inti/TD1Rest/Controller/MainController.java
@@ -1,13 +1,22 @@
 package com.inti.TD1Rest.Controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import com.inti.TD1Rest.Repository.EcoleRepository;
+import com.inti.TD1Rest.Repository.ProfesseurRepository;
 import com.inti.TD1Rest.Repository.UtilisateurRepository;
+import com.inti.TD1Rest.model.Ecole;
+import com.inti.TD1Rest.model.Etudiant;
+import com.inti.TD1Rest.model.Professeur;
 import com.inti.TD1Rest.model.Utilisateur;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +26,15 @@ public class MainController
 {
 	@Autowired
 	UtilisateurRepository ur;
+	
+	@Autowired
+	EcoleRepository ecoleR;
+	
+	@Autowired
+	ProfesseurRepository pr;
+	
+	
+	
 	
 	@GetMapping("/hello")
 	public String hello()
@@ -43,6 +61,23 @@ public class MainController
 		
 	}
 	
+	@PutMapping("/updateEcoleWithProfesseur/{idProf}/{idEcole}")
+	public String updateEcoleWithProfesseur(@PathVariable int idProf, @PathVariable int idEcole)
+	{
+		 Ecole ecole = ecoleR.getReferenceById(idEcole);
+		 
+		 Professeur prof = pr.getReferenceById(idProf);
+		 
+		 List<Professeur> listeProfesseurs = new ArrayList<Professeur>();
+		 
+		 listeProfesseurs.add(prof);
+		 
+		 ecole.setListeProfesseurs(listeProfesseurs);
+		 
+		 ecoleR.save(ecole);
+		
+		return "The school :" + ecole + " has been add";
+	}
 	
 	
 }
