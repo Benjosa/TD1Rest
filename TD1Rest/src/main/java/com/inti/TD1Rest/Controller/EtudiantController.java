fe34ead5ba30a472153c4b83d64664d7b05e6e79
jonathansablier <jonathansablier@macbook-pro-de-jonathan-2.home>
2022-05-30 13:10:51 +0200

    ajout de la fonction updateStudent

--- TD1Rest/src/main/java/com/inti/TD1Rest/Controller/EtudiantController.java
+++ TD1Rest/src/main/java/com/inti/TD1Rest/Controller/EtudiantController.java
@@ -10,7 +10,9 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -42,6 +44,28 @@ public class EtudiantController
 		
 		return new ResponseEntity<List<Etudiant>>(er.findAll(), HttpStatus.OK);
 	}
+	
+	@PutMapping("/updateStudent/{id}")
+	public String updateStudent(@RequestBody Etudiant etudiant, @PathVariable int id)
+	{
+		 Etudiant e1 = er.getReferenceById(id);
+		 
+		
+		 if(!e1.getNom().equals(etudiant.getNom()))
+		 {
+			 e1.setNom(etudiant.getNom());
+		 }
+		 if(!e1.getPrenom().equals(etudiant.getPrenom())&& etudiant.getPrenom()!= null)
+		 {
+			 e1.setPrenom(etudiant.getPrenom());
+		 }
+		
+		er.save(e1);
+		
+		return "The student :" + e1 + " has been update";
+	}
+	
+	
 	
 	@GetMapping("/deleteEtudiant")
 	public String deleteEtudiant()
